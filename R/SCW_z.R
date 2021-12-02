c0681058e899ace75ab0cee34facc180fba39697
cwbcm <57413825+cwbcm@users.noreply.github.com>
2021-12-02 17:16:34 -0600

    Add new function and update man page

--- R/SCW_z.R
+++ R/SCW_z.R
@@ -3,13 +3,15 @@
 #' @param pdb_file path to pdb file
 #' @param chain the protein chain SCW z score is calculated on
 #' @param dist_cutoff the distance cutoff that consider two atoms are adjacent
+#' @param resi a vector of residues numbers that are included in SCW bg calculation.
+#' If NULL, then all residues in that chain is used.
 #' @return a list with parameters that are needed for SCW z scores calculation
 #' @description Calculates the required stats for w_hat, w^2_hat calculation in
 #' SCW z scores. Residues are consider adjacent when any of their heavy atoms are
 #' within the cutoff range. The output is feed into ComputeSCWzscore function for
 #' z score calculation.
 #' @export
-GetSCWBackgound <- function(pdb_file, chain, dist_cutoff = 4) {
+GetSCWBackgound <- function(pdb_file, chain, dist_cutoff = 4, resi = NULL) {
   # This computes class counts used in calculation of
   # w_hat, w^2_hat in SCW z score.
   # It will produce values for both unbiased (1) and biased (j-i) approach
@@ -20,6 +22,10 @@ GetSCWBackgound <- function(pdb_file, chain, dist_cutoff = 4) {
   # output:
   #   list contains the distance df and the class count df
   cord <- GetCoordinates(pdb_file, chain, CA_only = FALSE)
+  if(!is.null(resi)) {
+    cord <- cord %>%
+      filter(POS %in% resi)
+  }
   dist <- GetDistanceMatrix(cord, output_type = "df") %>%
     mutate(A = ifelse(dist < dist_cutoff, 1, 0))
 
@@ -127,5 +133,62 @@ ComputeSCWzscore_list <- function(SCW_background, resi_list) {
   output <- tibble(condition = names(resi_list), resi = resi_list) %>%
     mutate(z = map(resi, ~ComputeSCWzscore(SCW_background, resi = .x, output_df = TRUE))) %>%
     unnest(cols = c(z))
+  return(output)
+}
+
+
+#' Compute SCW z scores for ET file
+#'
+#' @param pdb_file Path to pdb file, needs to be specified if SCW_bg is not provided
+#' @param chain Chain in the pdb file to use, needs to be specified if SCW_bg is not provided
+#' @param SCW_background A list with parameters required for SCW z score calculation.
+#' This is obtained through GetSCWBackground function. Needs to be specified if pdb_file/chain
+#' are not provided
+#' @param ET_file Path to ranks file
+#' @param coverage A numeric (0-1) value to specify the top fraction of ET residues are used
+#' in SCW z score calculation
+#' @param adjust_gap Logic. When False, this function behaves like PyET viewer, all residues
+#' in the pdb structure are used to calculate SCW background. When TRUE, only aligned positions
+#' in the pdb structure are used to calculate this background. Should set to TRUE when there is
+#' a large insertion in the pdb comparing to the ET sequence. Adjust_gap only works when using
+#' pdb_file/chain as input.
+#' @return a df with biased and unbiased SCW z scores.
+#' @description This function calculates the biased and unbiased SCW z scores
+#' for top residues in the ET ranks file. Can use pdb_file/chain or pre calculated SCW_background
+#' as input.
+#' @export
+ET_SCWzscore <- function(pdb_file = NULL, chain = NULL, SCW_background = NULL, ET_file,
+                     coverage = 0.3, adjust_gap = FALSE) {
+  ET <- ReadET(ET_file)
+  ET_seq <- paste0(ET$AA, collapse = "")
+  if (!is.null(SCW_background)) {
+    scw_bg <- SCW_background
+  } else {
+    if (is.null(pdb_file) | is.null(chain)) {
+      stop("Provide either pdb_file/chain or SCW_background")
+    }
+    resi_map <- CompareSeqs(pdb_file, chain, ET_seq, pos.only = TRUE)
+    if (adjust_gap == TRUE) {
+      scw_bg <- GetSCWBackgound(pdb_file = pdb_file, chain = chain, resi = resi_map$AA.POS.pdb)
+    } else {
+      scw_bg <- GetSCWBackgound(pdb_file = pdb_file, chain = chain)
+    }
+  }
+  # When pdb file is not provided, use residue numbers in the ET output to map to strucutre,
+  # if pdb file is provided, pdb seq is aligned with ET seq to map the ET top residues to
+  # structure.
+  if (!is.null(pdb_file)) {
+    resi_select <- ET %>%
+      left_join(rename(resi_map, POS = AA.POS.seq, pdb.POS = AA.POS.pdb), by = "POS")
+  } else {
+    resi_select <- ET %>%
+      mutate(pdb.POS = POS)
+  }
+  resi_select <- resi_select %>%
+    mutate(cov = rank(rho, ties.method = "max")/n()) %>%
+    filter(cov <= coverage) %>%
+    filter(!is.na(pdb.POS)) %>%
+    .$pdb.POS
+  output <- ComputeSCWzscore(SCW_background = scw_bg, resi = resi_select, output_df = TRUE)
   return(output)
 }
