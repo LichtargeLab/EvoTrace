0638207bfadfb9566020865e782b021f79a4943e
cwbcm <57413825+cwbcm@users.noreply.github.com>
2022-06-16 09:54:23 -0500

    Update README

---
+++ README.md
@@ -0,0 +1,12 @@
+# EvoTrace
+A collection of R functions to work on Evolutionary Trace (ET) related jobs.
+
+## Install
+```
+devtools::install_github("LichtargeLab/EvoTrace", build_vignettes = TRUE)
+```
+
+## Vignettes
+```
+browseVignettes("EvoTrace")
+```
---
+++ doc/Pymol_coloring.R
@@ -0,0 +1,72 @@
+## ---- echo = FALSE, message = FALSE-------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+options(tibble.print_min = 4, tibble.print_max = 4)
+
+## ----setup, message = FALSE---------------------------------------------------
+library(EvoTrace)
+
+## -----------------------------------------------------------------------------
+scales::show_col(SelectColor("ET"), labels = FALSE)
+ET <- ReadET(ET_file = "b0140.cov") 
+b0140.color <- ET %>%
+  select(POS, coverage) %>%
+  mutate(ET.color = SelectColor(color_type = "ET", prefix = "0x")[ceiling(coverage)])
+b0140.color
+
+## -----------------------------------------------------------------------------
+scales::show_col(SelectColor("alphafold"), labels = FALSE)
+
+## -----------------------------------------------------------------------------
+pLDDT <- GetpLDDT("b0140.pdb", chain = "A")
+b0140.color <- b0140.color %>%
+  left_join(pLDDT) %>%
+  mutate(pLDDT.color = SelectColor(color_type = "alphafold", prefix = "0x")[ceiling(pLDDT)])
+b0140.color
+
+## -----------------------------------------------------------------------------
+custom.color <- tibble(POS = c(139, 141), color = c("red", "cyan"))
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- PymolLoadFile("b0140.pdb")
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- c(pymol.cmd,
+               "set_name b0140, ET", 
+               "copy pLDDT, ET",
+               "copy custom_color, ET")
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- c(pymol.cmd,
+               PymolColorChainSingleColor(color = "white"))
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- c(pymol.cmd,
+               PymolColorChainByResidue(chain = "A", position = b0140.color$POS,
+                                        color = b0140.color$ET.color,
+                                        object = "ET"),
+               PymolColorChainByResidue(chain = "A", position = b0140.color$POS,
+                                        color = b0140.color$pLDDT.color,
+                                        object = "pLDDT"),
+               PymolColorChainByResidue(chain = "A", position = custom.color$POS,
+                                        color = custom.color$color,
+                                        object = "custom_color"))
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- c(pymol.cmd,
+               PymolSelectResidues("A", position = c(139, 141),
+                                   selection_name = "highlight"),
+               "show spheres, custom_color and highlight")
+
+## -----------------------------------------------------------------------------
+pymol.cmd <- c(pymol.cmd,
+               "set seq_view, 1",
+               "set grid_mode, 1",
+               "cmd._cmd._draw(cmd._COb)", # This pymol cmd is necessary for png to work with grid view in pymol batch mode.
+               "png b0140.png, width=5cm, height=5cm, dpi=300, ray=1")
+
+## -----------------------------------------------------------------------------
+PymolExecuteAndSave(pymol_cmd = pymol.cmd, output_file = "b0140_color.pse")
+
