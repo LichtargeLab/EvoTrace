09b0ab58ee938fa5e1ccd95ca433576303564572
cwbcm <57413825+cwbcm@users.noreply.github.com>
2023-03-03 23:50:51 -0600

    Update GetDistanceMatrix to report only non redundant pairs in the df output

--- R/GetDistanceMatrix.R
+++ R/GetDistanceMatrix.R
@@ -23,21 +23,32 @@ GetDistanceMatrix <- function(df, output_type = c("df", "matrix")) {
   #   between two residues
   # output: tibble or matrix
   output_type <- match.arg(output_type)
-  df_i <- df %>%
-    select(POS_i = POS, ATOM_i = ATOM, x_i = x, y_i = y, z_i = z)
-  df_j <- df %>%
-    select(POS_j = POS, ATOM_j = ATOM, x_j = x, y_j = y, z_j = z)
-  workdf <- expand_grid(df_i, df_j, .name_repair = "minimal") %>%
+  df <- mutate(df, id = 1:n())
+  workdf <- tibble(id_i = head(df$id, -1)) %>%
+    mutate(id_j = map(id_i, ~seq(.+1, nrow(df)))) %>%
+    unnest(cols = id_j)
+  workdf$POS_i <- df$POS[workdf$id_i]
+  workdf$POS_j <- df$POS[workdf$id_j]
+  workdf <- filter(workdf, POS_i != POS_j)
+  workdf$x_i <- df$x[workdf$id_i]
+  workdf$y_i <- df$y[workdf$id_i]
+  workdf$z_i <- df$z[workdf$id_i]
+  workdf$x_j <- df$x[workdf$id_j]
+  workdf$y_j <- df$y[workdf$id_j]
+  workdf$z_j <- df$z[workdf$id_j]
+  workdf <- workdf %>%
     mutate(dist = sqrt((x_i - x_j)^2 + (y_i - y_j)^2 + (z_i - z_j)^2)) %>%
     # mutate(dist = round(dist, 3)) %>%
     group_by(POS_i, POS_j) %>%
-    summarize(dist = min(dist)) %>%
+    summarize(dist = min(dist), .groups = "drop") %>%
     ungroup() %>%
     arrange(POS_i, POS_j)
   if (output_type == "matrix") {
-    resn <- length(unique(df$POS))
-    output <- workdf %>%
-      select(POS_i, POS_j, dist) %>%
+    resi <- unique(c(workdf$POS_i, workdf$POS_j))
+    output <- bind_rows(select(workdf, POS_i, POS_j, dist),
+                        select(workdf, POS_i = POS_j, POS_j = POS_i, dist),
+                        tibble(POS_i = resi, POS_j = resi, dist = 0)) %>%
+      arrange(POS_i, POS_j) %>%
       pivot_wider(names_from = POS_j, values_from = dist) %>%
       column_to_rownames("POS_i") %>%
       as.matrix()
