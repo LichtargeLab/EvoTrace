e6773e13d26229f597c661f11e04081ee05a5576
cwbcm <chen.wang@bcm.edu>
2023-03-27 22:02:58 -0500

    Add IQR_filter and update documents

---
+++ R/IQR_filter.R
@@ -0,0 +1,22 @@
+#' Apply 1.5 IQR filter for a given numeric vector
+#'
+#' @param x A given numeric vector to apply the filter
+#' @param type If "bool" is used, them a boolean vector with the same length as x is returned.
+#' If "ends" is used, then the lower and upper boundaries are returned
+#' @return A boolean vector or the lower and upper boundaries
+#' @description Apply IQR to vector x.
+#' @export
+
+IQR_filter <- function(x, type = c("bool", "ends")) {
+  type <- match.arg(type)
+  IQR <- quantile(x, 0.75) - quantile(x, 0.25)
+  ul <- quantile(x, 0.75) + 1.5*IQR
+  ll <- quantile(x, 0.25) - 1.5*IQR
+  if(type != "bool") {
+    output <- c(ll, ul)
+    names(output) <- NULL
+  } else {
+    output <- (x <= ul) & (x >= ll)
+  }
+  return(output)
+}
