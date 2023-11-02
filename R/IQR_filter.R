8467b4d198641387fdf58467479e1ac82f7ef1e5
cwbcm <chen.wang@bcm.edu>
2023-11-02 11:45:58 -0500

    Update IQR_filter with customizable fold.

--- R/IQR_filter.R
+++ R/IQR_filter.R
@@ -3,15 +3,16 @@
 #' @param x A given numeric vector to apply the filter
 #' @param type If "bool" is used, them a boolean vector with the same length as x is returned.
 #' If "ends" is used, then the lower and upper boundaries are returned
+#' @param fold Fold of IQR when computing the upper and lower boundaries.
 #' @return A boolean vector or the lower and upper boundaries
 #' @description Apply IQR to vector x.
 #' @export
 
-IQR_filter <- function(x, type = c("bool", "ends")) {
+IQR_filter <- function(x, type = c("bool", "ends"), fold = 1.5) {
   type <- match.arg(type)
   IQR <- quantile(x, 0.75) - quantile(x, 0.25)
-  ul <- quantile(x, 0.75) + 1.5*IQR
-  ll <- quantile(x, 0.25) - 1.5*IQR
+  ul <- quantile(x, 0.75) + fold*IQR
+  ll <- quantile(x, 0.25) - fold*IQR
   if(type != "bool") {
     output <- c(ll, ul)
     names(output) <- NULL
