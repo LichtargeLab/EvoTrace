c01e35bf6feb067c67ac5d3b8a048ffbeba57862
cwbcm <chen.wang@bcm.edu>
2025-08-13 09:08:47 -0500

    Make SUB3to1 to handel silent and del mutations in VEP output.

--- R/SUB3to1.R
+++ R/SUB3to1.R
@@ -5,13 +5,18 @@
 #' @description Convert amino acids from 3 letter to 1 letter in a substitution
 #' @export
 SUB3to1 <- function(SUB) {
-  ref <- str_sub(SUB, 1, 3) %>%
-    AA3to1()
+  ref <- str_sub(SUB, 1, 3)
   pos_loc <- str_locate(SUB, "[:digit:]+")
   pos <- str_sub(SUB, pos_loc[,1], pos_loc[,2])
-  alt <- str_sub(SUB, pos_loc[,2] + 1, pos_loc[,2] + 3) %>%
-    AA3to1()
+  alt <- str_sub(SUB, pos_loc[,2] + 1, pos_loc[,2] + 3)
+  # %3D are synonymous_variant labels in VEP
+  alt[alt == "%3D"] <- ref[alt == "%3D"]
+  ref1 <- AA3to1(ref)
+  alt1 <- AA3to1(alt)
+  # If 3 letter AA to 1 letter AA is not present (del),
+  # copy the letters intead.
+  alt1[is.na(alt1)] <- alt[is.na(alt1)]
   trailing <- str_sub(SUB, pos_loc[,2] + 4, -1)
-  output <- paste0(ref, pos, alt, trailing)
+  output <- paste0(ref1, pos, alt1, trailing)
   return(output)
 }
